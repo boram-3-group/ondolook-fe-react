7a35a32099a38aa17dd19b6ee7a14c4b78c8f939
junoo-park <junsoodev@gmail.com>
2025-04-05 01:16:06 +0900

    feat: tailwind 설정 수정

--- src/pages/Layouts/DefaultLayout.tsx
+++ src/pages/Layouts/DefaultLayout.tsx
@@ -1,7 +1,8 @@
 import { Outlet } from 'react-router-dom';
 export function DefaultLayout() {
   return (
-    <div className="default-layout p-4">
+    // py-12는 추후 header와 footer를 추가할 때 제거할 예정
+    <div className="w-full h-full py-12">
       <Outlet />
     </div>
   );
