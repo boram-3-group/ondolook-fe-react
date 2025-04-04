012d93b8142c344746bf9d58797c7824e764152d
junoo-park <junsoodev@gmail.com>
2025-04-05 00:17:47 +0900

    feat: html/home layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import './App.css';
+import './App.scss';
 import { Routes } from './pages/Routes';
 
 function App() {
   return (
-    <>
-      <Routes />
-    </>
+    <div className="app">
+      <div className="web-side__banner">
+        <div className="web-side__banner__content">
+          <h1>Welcome to our OndoLook!</h1>
+        </div>
+      </div>
+      <div className="mobile-content">
+        <Routes />
+      </div>
+    </div>
   );
 }
 
