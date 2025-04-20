f2a06aa8f71b70f3d9ca487720b971fd5739f4ab
junoo-park <junsoodev@gmail.com>
2025-04-21 02:49:35 +0900

    feat: 온보딩 페이지 퍼블 완료

--- src/pages/OnBoardPage/index.tsx
+++ src/pages/OnBoardPage/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-// import { useNavigate } from 'react-router-dom';
 import Carousel from '../../components/common/Carousel';
+import { Button } from '../../components/common/Button';
+import KakaoIcon from '../../assets/icons/icon-kakao-logo.svg';
+import GoogleIcon from '../../assets/icons/icon-google-logo.svg';
+import { useNavigate } from 'react-router-dom';
 
 export const OnBoardPage = () => {
   const images = [
@@ -13,11 +16,13 @@ export const OnBoardPage = () => {
     message: label,
   }));
 
+  const navigate = useNavigate();
+
   return (
     <div className="w-full max-w-xl mx-auto py-14">
       <Carousel
         slides={images.map(item => (
-          <div className="relative w-full h-full justify-center flex flex-col items-center">
+          <div className="relative w-full h-full justify-center flex flex-col items-center mb-8">
             <p
               className="text-[24px] font-bold leading-[150%] text-center
                 w-[159px]
@@ -28,12 +33,44 @@ export const OnBoardPage = () => {
             <img
               src={item.src}
               alt={item.alt}
-              className="w-full h-[310px] mt-5 pb-10 object-contain"
+              className="w-full h-[310px] mt-5 pb-5 object-contain"
               draggable={false}
             />
           </div>
         ))}
       />
+      <div className="mt-8 px-5 flex gap-3">
+        <Button intent="outline" size="large" side="full" className="w-1/2">
+          둘러보기
+        </Button>
+
+        <Button
+          intent="primary"
+          size="large"
+          side="full"
+          className="w-1/2"
+          onClick={() => navigate('/login/form')}
+        >
+          온도로 로그인
+        </Button>
+      </div>
+      <div className="my-5 flex items-center px-5">
+        <div className="flex-grow h-px bg-gray-200" />
+        <span className="mx-5 text-gray-400 text-sm">또는</span>
+        <div className="flex-grow h-px bg-gray-200" />
+      </div>
+      <div className="flex justify-center items-center gap-[28px]">
+        <button className="w-[52px] h-[52px] rounded-full flex items-center justify-center">
+          <img src={KakaoIcon} alt="카카오 로그인" />
+        </button>
+        <button className="w-[52px] h-[52px] rounded-full flex items-center justify-center">
+          <img src={GoogleIcon} alt="구글 로그인" />
+        </button>
+      </div>
+      <div className="mt-5 text-center text-sm">
+        <span className="text-gray-500">계정이 없으신가요? </span>
+        <button className="text-black font-semibold hover:underline">회원가입 하기</button>
+      </div>
     </div>
   );
 };
