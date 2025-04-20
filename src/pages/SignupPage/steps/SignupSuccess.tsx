eb098ce8d33007270f4cb89496282d643439d389
hyeon-aa <ahhyun1120@naver.com>
2025-04-20 23:29:13 +0900

    feat: 가입완료 페이지

--- src/pages/SignupPage/steps/SignupSuccess.tsx
+++ src/pages/SignupPage/steps/SignupSuccess.tsx
@@ -1,7 +1,15 @@
+import signUpStore from '../../../store/SignupStore';
+
 const SignupSuccess = () => {
+  const { username } = signUpStore(state => state);
+
   return (
     <>
-      <h1>회원가입 축하해</h1>
+      <div className="flex flex-col items-center justify-center">
+        <img src="/signupSuccess.svg" className="" />
+        <p className="text-Body2 text-primary-40">가입 완료!</p>
+        <div>{username}님, 환영해요</div>
+      </div>
     </>
   );
 };
