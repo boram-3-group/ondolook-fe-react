b771436ec95b965f92df6911c8fb52ea52ddc5bf
junoo-park <junsoodev@gmail.com>
2025-04-30 02:34:25 +0900

    feat: 환경변수23422

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { registerServiceWorker } from './utils/serviceWorker';
 import './index.css';
-import { requestPermissionAndGetToken } from './core/firebase.ts';
-
-// 서비스 워커 등록
-if (process.env.NODE_ENV === 'production') {
-  registerServiceWorker().then(() => {
-    // 서비스 워커 등록 후 Firebase 토큰 요청
-    requestPermissionAndGetToken();
-  });
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
