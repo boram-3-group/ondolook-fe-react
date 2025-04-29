7c1bdb92016fd6bcb86604778c3553e660fd8cfa
junoo-park <junsoodev@gmail.com>
2025-04-30 01:27:16 +0900

    feat: 환경변수

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
 console.log('firebase-messaging-sw.js loaded');
 firebase.initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
 });
 
 const messaging = firebase.messaging();
--- vite.config.ts
+++ vite.config.ts
@@ -90,14 +90,20 @@ export default defineConfig(({ mode }) => {
       },
     },
     define: {
-      'process.env.FIREBASE_API_KEY': JSON.stringify(env.VITE_APP_FIREBASE_API_KEY),
-      'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(env.VITE_APP_FIREBASE_AUTH_DOMAIN),
-      'process.env.FIREBASE_PROJECT_ID': JSON.stringify(env.VITE_APP_FIREBASE_PROJECT_ID),
-      'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(env.VITE_APP_FIREBASE_STORAGE_BUCKET),
-      'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(
+      'import.meta.env.VITE_APP_FIREBASE_API_KEY': JSON.stringify(env.VITE_APP_FIREBASE_API_KEY),
+      'import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN': JSON.stringify(
+        env.VITE_APP_FIREBASE_AUTH_DOMAIN
+      ),
+      'import.meta.env.VITE_APP_FIREBASE_PROJECT_ID': JSON.stringify(
+        env.VITE_APP_FIREBASE_PROJECT_ID
+      ),
+      'import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET': JSON.stringify(
+        env.VITE_APP_FIREBASE_STORAGE_BUCKET
+      ),
+      'import.meta.env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(
         env.VITE_APP_FIREBASE_MESSAGING_SENDER_ID
       ),
-      'process.env.FIREBASE_APP_ID': JSON.stringify(env.VITE_APP_FIREBASE_APP_ID),
+      'import.meta.env.VITE_APP_FIREBASE_APP_ID': JSON.stringify(env.VITE_APP_FIREBASE_APP_ID),
     },
   };
 });
