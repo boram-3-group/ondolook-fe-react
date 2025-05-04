67728f2392d5f2fdc40b28dff6a226ecbe5aa415
junoo-park <junsoodev@gmail.com>
2025-05-04 11:20:58 +0900

    feat: 배경 지정

--- src/pages/OnBoardPage/index.tsx
+++ src/pages/OnBoardPage/index.tsx
@@ -46,7 +46,7 @@ export const OnBoardPage = () => {
       />
       <div className="mt-7 px-5 flex gap-3">
         <Button intent="outline" size="large" side="full" className="w-1/2">
-          둘러보기
+          둘러보기1
         </Button>
 
         <Button
