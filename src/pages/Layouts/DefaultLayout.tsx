b457ac66b1e1b4dc1330442726ec38a895f3159b
junoo-park <junsoodev@gmail.com>
2025-05-04 16:04:03 +0900

    feat: hv- dvg

--- src/pages/Layouts/DefaultLayout.tsx
+++ src/pages/Layouts/DefaultLayout.tsx
@@ -6,13 +6,17 @@ export function DefaultLayout() {
   const { meta } = useRouteMeta();
 
   return (
-    <div className="w-full h-screen overflow-hidden">
+    <div
+      className="w-full h-screen overflow-hidden
+      flex flex-col
+    "
+    >
       <Header
         title={meta?.title}
         isShowBack={meta?.isShowBack}
         isShowForward={meta?.isShowForward}
       />
-      <div className="h-[calc(100%-44px)] overflow-hidden">
+      <div className="overflow-hidden flex-1">
         <Outlet />
       </div>
     </div>
